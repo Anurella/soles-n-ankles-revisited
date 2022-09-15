037038d3d42c3275b6cfee1c28c515d36546ea8a
Anurella <ndukwuamaka@yahoo.com>
2022-09-15 20:31:16 +0100

    select component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { COLORS, WEIGHTS , QUERIES } from '../../constants';
+import { COLORS, WEIGHTS, QUERIES } from '../../constants';
 import Logo from '../Logo';
 import SuperHeader from '../SuperHeader';
 import MobileMenu from '../MobileMenu';
@@ -9,87 +9,98 @@ import Icon from '../Icon';
 import UnstyledButton from '../UnstyledButton';
 
 const Header = () => {
-  const [showMobileMenu, setShowMobileMenu] = React.useState(false);
+	const [showMobileMenu, setShowMobileMenu] = React.useState(false);
 
-  // For our mobile hamburger menu, we'll want to use a button
-  // with an onClick handler, something like this:
-  //
-  // <button onClick={() => setShowMobileMenu(true)}>
+	// For our mobile hamburger menu, we'll want to use a button
+	// with an onClick handler, something like this:
+	//
+	// <button onClick={() => setShowMobileMenu(true)}>
 
-  return (
-    <header>
-      <SuperHeader />
-      <MainHeader>
-        <Side>
-          <Logo />
-        </Side>
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Side>
-            <IconWrapper>
-            <Icon id="shopping-bag"></Icon>
-            <Icon id="search"></Icon>
-            <UnstyledButton> <Icon id="menu"></Icon> </UnstyledButton>
-            </IconWrapper>
-          </Side>
-      </MainHeader>
+	return (
+		<header>
+			<SuperHeader />
+			<MainHeader>
+				<Side>
+					<Logo />
+				</Side>
+				<Nav>
+					<NavLink href="/sale">Sale</NavLink>
+					<NavLink href="/new">New&nbsp;Releases</NavLink>
+					<NavLink href="/men">Men</NavLink>
+					<NavLink href="/women">Women</NavLink>
+					<NavLink href="/kids">Kids</NavLink>
+					<NavLink href="/collections">Collections</NavLink>
+				</Nav>
+				<Side>
+					<IconWrapper>
+						<Icon id="shopping-bag"></Icon>
+						<Icon id="search"></Icon>
+						<UnstyledButton onClick={() => setShowMobileMenu(true)}>
+							{' '}
+							<Icon id="menu"></Icon>{' '}
+						</UnstyledButton>
+					</IconWrapper>
+				</Side>
+			</MainHeader>
 
-      <MobileMenu
-        isOpen={showMobileMenu}
-        onDismiss={() => setShowMobileMenu(false)}
-      />
-    </header>
-  );
+			<MobileMenu
+				isOpen={showMobileMenu}
+				onDismiss={() => setShowMobileMenu(false)}
+			/>
+		</header>
+	);
 };
 
 const MainHeader = styled.div`
-  display: flex;
-  align-items: baseline;
-  padding: 18px 32px;
-  height: 72px;
-  border-bottom: 1px solid ${COLORS.gray[300]};
+	display: flex;
+	align-items: baseline;
+	padding: 18px 32px;
+	height: 72px;
+	border-bottom: 1px solid ${COLORS.gray[300]};
 
-  @media ${QUERIES.tablets} {
-    border-top:4px solid ${COLORS.gray[900]};
-  }
+	@media ${QUERIES.tablets} {
+		border-top: 4px solid ${COLORS.gray[900]};
+	}
+
+	@media ${QUERIES.phones} {
+		padding: 20px 15px;
+	}
 `;
 
 const Nav = styled.nav`
-  display: flex;
-  gap: 48px;
-  margin: 0px 48px;
+	display: flex;
+	gap: 48px;
+	margin: 0px 48px;
 
-  @media ${QUERIES.tablets} {
-    display:none;
-  }
+	@media ${QUERIES.tablets} {
+		display: none;
+	}
 `;
 
 const Side = styled.div`
-  flex: 1;
+	flex: 1;
 `;
 
 const NavLink = styled.a`
-  font-size: 1.125rem;
-  text-transform: uppercase;
-  text-decoration: none;
-  color: ${COLORS.gray[900]};
-  font-weight: ${WEIGHTS.medium};
+	font-size: 1.125rem;
+	text-transform: uppercase;
+	text-decoration: none;
+	color: ${COLORS.gray[900]};
+	font-weight: ${WEIGHTS.medium};
 
-  &:first-of-type {
-    color: ${COLORS.secondary};
-  }
+	&:first-of-type {
+		color: ${COLORS.secondary};
+	}
 `;
 
 const IconWrapper = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  gap: 37px;
+	display: flex;
+	justify-content: flex-end;
+	gap: 37px;
+
+	@media ${QUERIES.phones} {
+		gap: 20px;
+	}
 `;
 
 export default Header;
